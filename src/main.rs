27855fa7601fde8323919b759b0e390a00ef30ad
SEFA1905 <70607438+SEFA1905@users.noreply.github.com>
2023-02-17 02:01:42 +0300

    tcp address listener has been added

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,14 @@ const BARBAROSSA_SERVER_ADDRESS:&str = "127.0.0.1:8000";
 
 fn main() {
 
-       println!("barbaroosa starting: [{}]",BARBAROSSA_SERVER_ADDRESS); 
-       let listener =  TcpListener::bind(BARBAROSSA_SERVER_ADDRESS);
+       println!("barbaroosa starting:  [{}]",BARBAROSSA_SERVER_ADDRESS); 
+       let listener =  TcpListener::bind(BARBAROSSA_SERVER_ADDRESS).unwrap();
+       println!("barbaroosa listining: [{}]",BARBAROSSA_SERVER_ADDRESS); 
+       
+       for stream in listener.incoming(){
+            let _stream = stream.unwrap();
+            println!("connection established!");
+       }
 
 
 }
